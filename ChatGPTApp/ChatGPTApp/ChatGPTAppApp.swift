5d753b22dc5a5ad0e55479012a2fad2f2f27bf04
Jesus Galaz <jesusgalazr@icloud.com>
2024-03-10 16:12:00 -0600

    Carpeta con aplicación de Chatbot usando GPT Api

---
+++ ChatGPTApp/ChatGPTApp/ChatGPTAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTAppApp.swift
+//  ChatGPTApp
+//
+//  Created by Jesus Alonso Galaz Reyes on 09/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ChatView()
+        }
+    }
+}
---
+++ ChatGPTApp/ChatGPTApp/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  ChatGPTApp
+//
+//  Created by Jesus Alonso Galaz Reyes on 09/03/24.
+//
+
+import Foundation
+
+enum Constants {
+    static let openAIApiKey = ProcessInfo.processInfo.environment["OPENAI_API_KEY"]!
+}
+

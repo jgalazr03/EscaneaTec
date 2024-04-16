028fb4eb4585236b4080e7e6ab4da802fde8da63
EmilianoA0157 <107271741+EmilianoA0157@users.noreply.github.com>
2024-04-16 14:04:52 -0600

    PulmoCare Completo
    Tiene las nuevas vistas del ModeloML y Result View

---
+++ EscaneaTec 2/Reto_Inicial/Reto_Inicial/Views/Respuesta.swift
@@ -0,0 +1,12 @@
+//
+//  Respuesta.swift
+//  Reto_Inicial
+//
+//  Created by Jesus Alonso Galaz Reyes on 11/04/24.
+//
+
+import Foundation
+
+struct Respuesta : Codable {
+    var userID : [UserDetail]
+}
